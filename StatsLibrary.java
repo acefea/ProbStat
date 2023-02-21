51a6d4961f7ab4ea2decdae1acfd9392c9a7da88
acefea <acefea@go.stockton.edu>
2023-02-21 16:26:22 -0500

    sets, fact, perm, comb 2/21/23

--- StatsLibrary.java
+++ StatsLibrary.java
@@ -1,11 +1,12 @@
 //Program written by Leonardo Acefe for CSCI3327 Section 001 Probability and Applied Statistics taught by Byron Hoy (Spring 2023).
 import java.util.ArrayList;
 import java.lang.Math;
+import java.math.BigInteger;
 public class StatsLibrary{
     //This project is responsible for processing common probability and statistics problems to aid in our homework assignments.
-    //Methods include mean, median, mode, and standard deviation
-    //Union, intersection, complement of two ArrayLists<Double>
-    //Factorial, permutation and combinations calculator (using BigInteger from Java API)
+    //Methods include mean, median, mode, and standard deviation X
+    //Union, intersection, complement of two ArrayLists<Integer> X
+    //Factorial, permutation and combinations calculator (using BigInteger from Java API) X
     //Monte Carlo problem (answer 2.20 on page 34 in comments) over 10,000 generations
     //Birthday problem 
     //Excel work from beginning of semester
@@ -14,18 +15,18 @@ public class StatsLibrary{
     //All work must be within the Github, submit the Github link on Blackboard.
     //Optional EC: Word document labeled with extra credit considerations
 
-    //This method solves for and returns the average (sum of elements divided by number of elements) of an inputted ArrayList of Integers.
-    public double mean(ArrayList<Integer> input){
+    //This method solves for and returns the average (sum of elements divided by number of elements) of an inputted ArrayList of Doubles.
+    public double mean(ArrayList<Double> input){
         double sum = 0; //Average will be 0 if inputted ArrayList is empty.
-        for(int element:input){
+        for(double element:input){
             sum+=element;
         }
         return (sum/input.size());
     }
 
-    //This method solves for and returns the median (middle) of an inputted ArrayList of Integers.
-    public Integer median(ArrayList<Integer> input){
-        Integer median = 0;
+    //This method solves for and returns the median (middle) of an inputted ArrayList of Doubles.
+    public double median(ArrayList<Double> input){
+        double median = 0;
         //If inputted array has an odd number of elements, there is a defined median.
         if((input.size()%2)!=0){
             int midpoint = (input.size()/2);
@@ -33,18 +34,18 @@ public class StatsLibrary{
         }
         //Otherwise, and array with an even number of elements will not have a median.
         else{
-            median = null; //No median -> return null
+            median = -1.0; //No median -> return -1.0
         }
         return median;
     }
 
-    //This method finds the mode (most frequent element) of an inputted ArrayList of Integers.
-    //Code mostly taken from this stackoverflow post: https://stackoverflow.com/a/36416334
-    public Integer mode(ArrayList<Integer> input){
-        Integer mode = 0;
+    //This method finds the mode (most frequent element) of an inputted ArrayList of Doubles.
+    //Note: Code mostly taken from this stackoverflow post: https://stackoverflow.com/a/36416334
+    public double mode(ArrayList<Double> input){
+        double mode = 0;
         int maxFrequency = 0;
         for(int x = 0; x < input.size(); x++){
-            Integer value = input.get(x);
+            double value = input.get(x);
             int frequency = 0;
             for(int y = 0; y < input.size(); y++){
                 if (input.get(y)==value) {
@@ -57,40 +58,98 @@ public class StatsLibrary{
             }
         }
         if(maxFrequency<1){
-            mode = null; //No mode -> return null
+            mode = -1.0; //No mode -> return -1.0
         }
         return mode;
     }
 
-    //This method finds the standard deviation of an ArrayList of Integers.
-    //Figure out a way to make this more accurate with the use of doubles.
-    public double standardDeviation(ArrayList<Integer> input){
+    //This method finds the standard deviation of an ArrayList of Doubles.
+    public double standardDeviation(ArrayList<Double> input){
         double std = 0;
+        double sum = 0;
         //Step 1: Find the mean
-        double sum = 0; //Average will be 0 if inputted ArrayList is empty.
-        for(int element:input){
-            sum+=element;
-        }
-        double mean = (sum/input.size());
+        double mean = mean(input);
         //Step 2: Subtract mean from all elements
         for(int i=0;i > input.size();i++){
-            Double element = input.get(i) - mean;
-            Integer value = element.intValue();
-            input.set(i, value);
+            double element = input.get(i) - mean;
+            input.set(i, element);
         }
         //Step 3: Square these deviations
         for(int i=0;i > input.size();i++){
-            Integer element = input.get(i) * input.get(i); 
+            double element = input.get(i) * input.get(i); 
             input.set(i, element);
         }
         //Step 4: Sum these squares
-        for(int element:input){
-            sum+=element;
+        for(double x:input){
+            sum+=x;
         }
-        //Step 5: Divide by count
+        //Step 5: Divide by count-1
         std = sum/input.size();
         //Step 6: Square root
         std = Math.sqrt(std);
         return std;
+    }
+
+    //This method finds the factorial of a given integer value
+    //Note: I was having issues with visibility, so I referenced the code here: https://javarevisited.blogspot.com/2015/08/how-to-calculate-large-factorials-using-BigInteger-Java-Example.html 
+    public BigInteger factorial(int input){
+        BigInteger result = BigInteger.valueOf(1);
+            for(int i=input;i>0;i--){
+                result = result.multiply(BigInteger.valueOf(i)); 
+            }
+        return result;
+    }    
+
+    //This method finds the permutation of a given selection and total
+    public BigInteger permutation(int n, int r){
+        BigInteger p = factorial(n).divide(factorial(n-r));
+        return p;
+    }
+
+    //This method finds the combination of a given selection and total
+    public BigInteger combination(int n, int r){
+        BigInteger c = factorial(n).divide(factorial(r).multiply(factorial(n-r)));
+        return c;
+    }
+
+    //This method finds the union of two inputted ArrayLists of Integers, a and b.
+    public ArrayList<Integer> union(ArrayList<Integer> a, ArrayList<Integer> b){
+        ArrayList<Integer> result = new ArrayList<Integer>();
+        for(int i=0;i<a.size();i++){
+            if(!result.contains(a.get(i))){
+               result.add(a.get(i)); 
+            }
+        }
+        for(int i=0;i<b.size();i++){
+            if(!result.contains(b.get(i))){
+              result.add(b.get(i));  
+            }
+        }
+        return result;
+    }
+
+    //This method finds the intersection of two inputted ArrayLists of Integers, a and b, in sample space s.
+    public ArrayList<Integer> intersection(ArrayList<Integer> a, ArrayList<Integer> b){
+        ArrayList<Integer> result = new ArrayList<Integer>();
+        for(int i=0;i<a.size();i++){
+            if(b.contains(a.get(i))){
+                result.add(a.get(i));
+            }
+        }
+        return result;
+    }
+
+    //This method finds the compliment of inputted ArrayList of Integers, a, against the inputted total sample space, s.
+    public ArrayList<Integer> complement(ArrayList<Integer> a, ArrayList<Integer> s){
+        ArrayList<Integer> result = new ArrayList<Integer>();
+        for(int i=0;i<s.size();i++){
+            result.add(s.get(i));
+        }
+        for(int i=0;i<a.size();i++){
+            if(result.contains(a.get(i))){
+                result.remove(a.get(i));
+            }
+        }
+        return result;
     }
 }
--- TestStatsLibrary.java
+++ TestStatsLibrary.java
@@ -3,16 +3,42 @@ import java.util.ArrayList;
 public class TestStatsLibrary {
     public static void main(String[] args){
         StatsLibrary test = new StatsLibrary();
-        ArrayList<Integer> numbers = new ArrayList<>();
-        numbers.add(46);
-        numbers.add(69);
-        numbers.add(32);
-        numbers.add(60);
-        numbers.add(52);
-        numbers.add(41);
+        
+        ArrayList<Double> numbers = new ArrayList<>(); //numbers = {46, 69, 32, 60, 52, 41}
+        numbers.add(46.0);
+        numbers.add(69.0);
+        numbers.add(32.0);
+        numbers.add(60.0);
+        numbers.add(52.0);
+        numbers.add(41.0);
         System.out.println("Mean: "+test.mean(numbers));
         System.out.println("Median: "+test.median(numbers));
         System.out.println("Mode: "+test.mode(numbers));
         System.out.println("Standard Deviation: "+test.standardDeviation(numbers));
+
+        int n = 12;
+        int r = 2;
+        System.out.println("Factorial of "+n+": "+test.factorial(n));
+        System.out.println("Permutation of n = "+n+" and r = "+r+": "+test.permutation(n,r));
+        System.out.println("Combination of n = "+n+" and r = "+r+": "+test.combination(n,r));
+
+        ArrayList<Integer> a = new ArrayList<>();
+        ArrayList<Integer> b = new ArrayList<>();
+        ArrayList<Integer> s = new ArrayList<>();
+        s.add(1);
+        s.add(2);
+        s.add(3);
+        s.add(4);
+        s.add(5); //total s = {1,2,3,4,5}
+        a.add(2);
+        a.add(3);
+        a.add(5); //a = {2,3,5} such that a' = {1,4}
+        b.add(1);
+        b.add(2);
+        b.add(4); //b = {1,2,4} such that b' = {3,5}
+        System.out.println("Union of A and B: "+test.union(a,b));
+        System.out.println("Intersection of A and B: "+test.intersection(a,b));
+        System.out.println("Complement of A: "+test.complement(a, s));
+        System.out.println("Complement of B: "+test.complement(b, s));
     }
 }
